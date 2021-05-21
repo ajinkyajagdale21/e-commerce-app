bca260ac7ab67491d130f660b859ad4033d13875
ajinkyajagdale21 <68507834+ajinkyajagdale21@users.noreply.github.com>
2021-05-21 20:10:46 +0530

    update: productCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {useData} from '../Contexts/DataContext';
 
 export const ProductCard=({product})=>{
-  const {state:{cart,wishList},dispatch}=useData()
+
   const {productName,image,price,ratings,_id} = product;
     return(
       <div>
